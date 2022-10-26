f6bf419561274e2573def80ddb5357de1a7d6a94
aggagah <gagahsp4@gmail.com>
2022-10-26 13:34:52 +0700

    add routing for register page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </Router>
