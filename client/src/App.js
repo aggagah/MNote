9f53d20643ca0780d427cdecd40128d0b7210134
aggagah <gagahsp4@gmail.com>
2022-10-29 02:56:46 +0700

    routing sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 // import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+// import Sidebar from "./components/Sidebar";
+// import Dashboard from "./pages/Dashboard";
+// import Help from "./pages/Help";
+import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+// import Search from "./pages/Search";
+// import Settings from "./pages/Settings";
+// import Summary from "./pages/Summary";
 
 function App() {
     return (
         <Router>
+            {/* <Sidebar /> */}
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/mnote/*" element={<Homepage />} />
+                {/* <Route path="/summary" element={<Homepage />} />
+                <Route path="/search" element={<Homepage />} />
+                <Route path="/settings" element={<Homepage />} />
+                <Route path="/help" element={<Homepage />} /> */}
             </Routes>
         </Router>
     );
